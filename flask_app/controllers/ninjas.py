62352905061cc9ef13d8cc9a458db439ceaa5b21
Camila Castro <109989986+CamCasG@users.noreply.github.com>
2022-09-29 10:30:32 -0300

    Update ninjas.py

--- flask_app/controllers/ninjas.py
+++ flask_app/controllers/ninjas.py
@@ -10,6 +10,18 @@ def ninjas():
 
 @app.route('/ninjas/crear', methods=['POST'])
 def crear_ninja():
-    print(request.form)
-    Ninja.save(request.form)
-    return redirect('/ninjas')
+    print(request.form) 
+    id_ninja = Ninja.save(request.form)
+
+    data = {
+        "id_ninja":id_ninja
+    }
+    
+    resultado = Ninja.get_un_ninja(data)
+    print(resultado, "SI TODO ESTA BIEN")
+    return redirect(f'/dojos/{resultado["dojo_id"]}')
+
+# @app.route('/dojos/tabla')
+# def mostrar_ninjas_en_dojos():
+
+#     return render_template("dojos.html",dojo=Dojo.get_all())
